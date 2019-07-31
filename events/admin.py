0ff7caa89acbb13ab32a9ba0ea5bbb958e6dd9c4
Marc Schwartz <mathemaat@xs4all.nl>
2019-07-31 17:44:48 +0200

    Apply proof of concept of the main overview

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Doodsoorzaak, OnderliggendeOorzaak, Gebeurtenis, Dier
+
+
+class DoodsoorzaakAdmin(admin.ModelAdmin):
+    list_display = ('omschrijving',)
+    list_display_links = ('omschrijving',)
+
+
+class OnderliggendeOorzaakAdmin(admin.ModelAdmin):
+    list_display = ('omschrijving', 'doodsoorzaak',)
+    list_display_links = ('omschrijving',)
+
+
+class DierAdmin(admin.ModelAdmin):
+    list_display = ('omschrijving', 'afbeelding', 'rekeneenheid',)
+    list_display_links = ('omschrijving',)
+
+
+admin.site.register(Doodsoorzaak, DoodsoorzaakAdmin)
+admin.site.register(OnderliggendeOorzaak, OnderliggendeOorzaakAdmin)
+admin.site.register(Dier, DierAdmin)
+admin.site.register(Gebeurtenis)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,129 @@
 from django.db import models
 
-# Create your models here.
+
+class Doodsoorzaak(models.Model):
+    omschrijving = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Doodsoorzaak"
+        verbose_name_plural = "Doodsoorzaken"
+
+    def __str__(self):
+        return self.omschrijving
+
+
+class OnderliggendeOorzaak(models.Model):
+    doodsoorzaak = models.ForeignKey(Doodsoorzaak, on_delete=models.PROTECT)
+    omschrijving = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Onderliggende oorzaak"
+        verbose_name_plural = "Onderliggende oorzaken"
+
+    def __str__(self):
+        return "{} ({})".format(self.doodsoorzaak.omschrijving, self.omschrijving)
+
+
+class Dier(models.Model):
+
+    ICON_FOLDER = 'dieren/'
+    ICON_FOLDER_GIGA = '128x128/'
+    ICON_FOLDER_MEGA = '64x64/'
+    ICON_FOLDER_GROOT = '32x32/'
+
+    omschrijving = models.CharField(max_length=255)
+    afbeelding = models.CharField(max_length=16, default='')
+    rekeneenheid = models.IntegerField(default=1)
+
+    class Meta:
+        verbose_name = "Dier"
+        verbose_name_plural = "Dieren"
+
+    def __str__(self):
+        return self.omschrijving
+
+
+class Gebeurtenis(models.Model):
+
+    OMVANG_UNKNOWN = 'UNKNOWN'
+    OMVANG_GIGA = 'GIGA'
+    OMVANG_MEGA = 'MEGA'
+    OMVANG_GROOT = 'GROOT'
+
+    datum = models.DateField()
+    locatie = models.CharField(max_length=255)
+    oorzaak = models.ForeignKey(OnderliggendeOorzaak, on_delete=models.PROTECT)
+    dier = models.ForeignKey(Dier, on_delete=models.PROTECT, null=True, blank=True)
+    slachtofferaantal = models.IntegerField(null=True, blank=True)
+    slachtofferbeschrijving_format = models.CharField(max_length=255, default='{slachtofferaantal} {dier}')
+
+
+    class Meta:
+        verbose_name = "Gebeurtenis"
+        verbose_name_plural = "Gebeurtenissen"
+
+    def __str__(self):
+        return "{}: {} te {} wegens {}".format(
+            self.datum.strftime("%d-%m-%Y"),
+            self.get_slachtofferbeschrijving(),
+            self.locatie,
+            self.oorzaak
+        )
+
+    def get_slachtofferbeschrijving(self):
+        return self.slachtofferbeschrijving_format.format(
+            slachtofferaantal=self.slachtofferaantal,
+            dier=self.dier.omschrijving.lower()
+        )
+
+    def get_omvang(self):
+        if (self.slachtofferaantal is None):
+            return self.OMVANG_UNKNOWN
+        elif (self.slachtofferaantal >= self.dier.rekeneenheid * 100):
+            return self.OMVANG_GIGA
+        elif (self.slachtofferaantal >= self.dier.rekeneenheid * 10):
+            return self.OMVANG_MEGA
+        else:
+            return self.OMVANG_GROOT
+
+    def get_rekeneenheden(self):
+        return {
+            self.OMVANG_GIGA: self.dier.rekeneenheid * 100,
+            self.OMVANG_MEGA: self.dier.rekeneenheid * 10,
+            self.OMVANG_GROOT: self.dier.rekeneenheid,
+        }
+
+    def get_afbeeldingsaantallen(self):
+        afbeeldingaantallen = {
+            self.OMVANG_GIGA: 0,
+            self.OMVANG_MEGA: 0,
+            self.OMVANG_GROOT: 0,
+        }
+        rekeneenheden = self.get_rekeneenheden()
+
+        omvang = self.get_omvang()
+        if (omvang == self.OMVANG_UNKNOWN):
+            return afbeeldingaantallen
+        else:
+            rest = self.slachtofferaantal
+            for omvang in rekeneenheden:
+                rekeneenheid = rekeneenheden[omvang]
+                while (rest >= rekeneenheid):
+                    afbeeldingaantallen[omvang] += 1
+                    rest -= rekeneenheid
+            return afbeeldingaantallen
+
+    def get_afbeeldingenreeks(self):
+        aantallen = self.get_afbeeldingsaantallen()
+        folders = {
+            self.OMVANG_GIGA: Dier.ICON_FOLDER_GIGA,
+            self.OMVANG_MEGA: Dier.ICON_FOLDER_MEGA,
+            self.OMVANG_GROOT: Dier.ICON_FOLDER_GROOT,
+        }
+
+        afbeeldingen = []
+        for omvang in aantallen:
+            aantal = aantallen[omvang]
+            path = Dier.ICON_FOLDER + folders[omvang] + self.dier.afbeelding
+            afbeeldingen += [path] * aantal
+        return afbeeldingen
