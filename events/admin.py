09b9a2a2a2077aab9025186a9e39380c04bac1ac
Marc Schwartz <mathemaat@xs4all.nl>
2019-08-05 16:16:51 +0200

    Update admin page of events

--- events/admin.py
+++ events/admin.py
@@ -17,8 +17,11 @@ class DierAdmin(admin.ModelAdmin):
     list_display = ('omschrijving', 'afbeelding', 'rekeneenheid',)
     list_display_links = ('omschrijving',)
 
+class GebeurtenisAdmin(admin.ModelAdmin):
+    list_display = ('datum', 'locatie', 'slachtofferaantal', 'dier', 'oorzaak', 'is_bevestigd',)
+    list_display_links = ('datum',)
 
 admin.site.register(Doodsoorzaak, DoodsoorzaakAdmin)
 admin.site.register(OnderliggendeOorzaak, OnderliggendeOorzaakAdmin)
 admin.site.register(Dier, DierAdmin)
-admin.site.register(Gebeurtenis)
+admin.site.register(Gebeurtenis, GebeurtenisAdmin)
